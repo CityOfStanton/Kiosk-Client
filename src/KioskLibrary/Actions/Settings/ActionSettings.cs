807ca940528f7dc48e7b17466e998373a4008f26
Chad Birch <chadbirch@hotmail.com>
2020-12-21 16:20:24 -0500

    Added library with potential actions (#4)

---
+++ src/KioskLibrary/Actions/Settings/ActionSettings.cs
@@ -0,0 +1,14 @@
+﻿/*  
+ * Copyright 2020
+ * City of Stanton
+ * Stanton, Kentucky
+ * www.stantonky.gov
+ */
+
+namespace KioskLibrary.Actions.Settings
+{
+    /// <summary>
+    /// The settings for a particular type of <see cref="Action"/>
+    /// </summary>
+    public record ActionSettings { }
+}
---
+++ src/KioskLibrary/Actions/Settings/ImageSettings.cs
@@ -0,0 +1,17 @@
+﻿/*  
+ * Copyright 2020
+ * City of Stanton
+ * Stanton, Kentucky
+ * www.stantonky.gov
+ */
+
+using KioskLibrary.Actions;
+
+namespace KioskLibrary.Actions.Settings
+{
+    /// <summary>
+    /// Settings for the <see cref="ActionType.Image"/> <see cref="ActionType"/>.
+    /// </summary>
+    public record ImageSettings(string Path, string Scale)
+        : ActionSettings;
+}
