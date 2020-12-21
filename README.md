2b931942d8069daf19f4a89647b5cd8bc82aa077
Chad Birch <chadbirch@hotmail.com>
2020-12-21 16:39:56 -0500

    Added CodeQL analysis to project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kiosk Client
+
+![CodeQL](https://github.com/CityOfStanton/Kiosk-Client/workflows/CodeQL/badge.svg)
+
 UWP app used for various displays with our Windows 10 systems running in Kiosk mode.
--- src/KioskClient/App.xaml.cs
+++ src/KioskClient/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Kiosk_Client
+namespace KioskClient
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
