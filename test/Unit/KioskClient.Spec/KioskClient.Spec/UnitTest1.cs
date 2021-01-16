e3dfb6d3d7e9cd43643d741fc58d8bb95e0abc3e
Chad Birch <chadbirch@hotmail.com>
2021-01-15 19:48:36 -0500

    Added test projects

---
+++ test/Unit/KioskClient.Spec/KioskClient.Spec/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KioskClient.Spec
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/Unit/KioskLibrary.Spec/KioskLibrary.Spec/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KioskLibrary.Spec
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
