0d1fa8315423282b1bb0adae7edc9a52290ede10
Chad Birch <chadbirch@hotmail.com>
2021-02-13 14:55:15 -0500

    Added new converters

---
+++ src/KioskClient/Converters/BooleanToInvertedVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright 2021
+ * City of Stanton
+ * Stanton, Kentucky
+ * www.stantonky.gov
+ * github.com/CityOfStanton
+ */
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace KioskLibrary.Converters
+{
+    /// <summary>
+    /// Inverts a boolean value , then converts it to a <see cref="Visibility"/>
+    /// </summary>
+    /// <remarks><see cref="false"/> is converted to <see cref="Visibility.Visible"/> and <see cref="true"/> is converted to <see cref="Visibility.Collapsed"/></remarks>
+    public class BooleanToInvertedVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert the boolean value to a <see cref="Visibility"/>
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, string language) => (bool)value ? Visibility.Collapsed : Visibility.Visible;
+
+        /// <summary>
+        /// Restores a converted boolean value to a <see cref="Visibility"/>
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => (Visibility)value != Visibility.Visible;
+    }
+}
---
+++ src/KioskClient/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright 2021
+ * City of Stanton
+ * Stanton, Kentucky
+ * www.stantonky.gov
+ * github.com/CityOfStanton
+ */
+
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace KioskLibrary.Converters
+{
+    /// <summary>
+    /// Converts a boolean value to a <see cref="Visibility"/>
+    /// </summary>
+    /// <remarks><see cref="true"/> is converted to <see cref="Visibility.Visible"/> and <see cref="false"/> is converted to <see cref="Visibility.Collapsed"/></remarks>
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert the boolean value to a <see cref="Visibility"/>
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, string language) => (bool)value ? Visibility.Visible : Visibility.Collapsed;
+
+        /// <summary>
+        /// Restores a converted boolean value to a <see cref="Visibility"/>
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => (Visibility)value == Visibility.Visible;
+    }
+}
